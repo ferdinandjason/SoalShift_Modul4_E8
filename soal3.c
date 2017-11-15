a57f94c0364bdd9d4f082e906817943712661a25
Ferdinand Jason G <ferdinandjasong@gmail.com>
2017-11-15 14:42:02 +0700

    Commit soal3.c fixed(?)

--- soal3.c
+++ soal3.c
@@ -11,104 +11,106 @@
 static const char *dirpath = "/home/ferdinand/Downloads/tmp";
 
 static int xmp_getattr(const char *path, struct stat *stbuf)
-{
-  int res;
-	char fpath[1000];
-	sprintf(fpath,"%s%s",dirpath,path);
-	res = lstat(fpath, stbuf);
-
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-		       off_t offset, struct fuse_file_info *fi)
-{
-  char fpath[1000];
-	if(strcmp(path,"/") == 0)
-	{
-		path=dirpath;
-		sprintf(fpath,"%s",path);
+  {
+   int res;
+ 	char fpath[1000];
+ 	sprintf(fpath,"%s%s",dirpath,path);
+ 	res = lstat(fpath, stbuf);
+ 
+ 	if (res == -1)
+ 		return -errno;
+ 
+ 	return 0;
+ }
+ 
+ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+ 		       off_t offset, struct fuse_file_info *fi)
+ {
+   char fpath[1000];
+ 	if(strcmp(path,"/") == 0)
+ 	{
+ 		path=dirpath;
+ 		sprintf(fpath,"%s",path);
+ 	}
+ 	else sprintf(fpath, "%s%s",dirpath,path);
+ 	int res = 0;
+ 
+ 	DIR *dp;
+ 	struct dirent *de;
+ 
+ 	(void) offset;
+ 	(void) fi;
+ 
+ 	dp = opendir(fpath);
+ 	if (dp == NULL)
+ 		return -errno;
+ 
+ 	while ((de = readdir(dp)) != NULL) {
+ 		struct stat st;
+ 		memset(&st, 0, sizeof(st));
+ 		st.st_ino = de->d_ino;
+ 		st.st_mode = de->d_type << 12;
+ 		res = (filler(buf, de->d_name, &st, 0));
+ 			if(res!=0) break;
+ 	}
+ 
+ 	closedir(dp);
+ 	return 0;
+ }
+ 
+ static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
+ 		    struct fuse_file_info *fi)
+ {
+   char fpath[1000];
+ 	if(strcmp(path,"/") == 0)
+ 	{
+ 		path=dirpath;
+ 		sprintf(fpath,"%s",path);
+ 	}
+ 	else sprintf(fpath, "%s%s",dirpath,path);
+ 	int res = 0;
+   int fd = 0 ;
+ 
+ 	(void) fi;
+ 	fd = open(fpath, O_RDONLY);
+ 	if (fd == -1)
+ 		return -errno;
+ 
+ 	res = pread(fd, buf, size, offset);
+ 	if (res == -1)
+ 		res = -errno;
+ 
+ 	close(fd);
+  	return res;
+  }
+  
+ static int xmp_rename(const char *from, const char *to)
+ {
+     // juga digunakan untuk menyimpan / menyimpan hasil perubahan isi berkas
+     int res;
+     char ffrom[1000];
+     char tto[1000];
+     system("mkdir /home/ferdinand/Downloads/tmp/simpanan -p");
+     char direk[] = "/home/ferdinand/Downloads/tmp/simpanan";
+     sprintf(ffrom,"%s%s",dirpath,from);
+     sprintf(tto,"%s%s",direk,to);
+	 res = rename(ffrom, tto);
+	 char command[1000];
+	 sprintf(command,"cp %s %s",ffrom,tto);
+	 system(command);
+     if(res == -1)
+     	return -errno;
+ 
+     return 0;
 	}
-	else sprintf(fpath, "%s%s",dirpath,path);
-	int res = 0;
-
-	DIR *dp;
-	struct dirent *de;
-
-	(void) offset;
-	(void) fi;
-
-	dp = opendir(fpath);
-	if (dp == NULL)
-		return -errno;
-
-	while ((de = readdir(dp)) != NULL) {
-		struct stat st;
-		memset(&st, 0, sizeof(st));
-		st.st_ino = de->d_ino;
-		st.st_mode = de->d_type << 12;
-		res = (filler(buf, de->d_name, &st, 0));
-			if(res!=0) break;
-	}
-
-	closedir(dp);
-	return 0;
-}
-
-static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
-		    struct fuse_file_info *fi)
-{
-  char fpath[1000];
-	if(strcmp(path,"/") == 0)
-	{
-		path=dirpath;
-		sprintf(fpath,"%s",path);
-	}
-	else sprintf(fpath, "%s%s",dirpath,path);
-	int res = 0;
-    int fd = 0 ;
-
-	(void) fi;
-	fd = open(fpath, O_RDONLY);
-	if (fd == -1)
-		return -errno;
-
-	res = pread(fd, buf, size, offset);
-	if (res == -1)
-		res = -errno;
-
-	close(fd);
-	return res;
-}
-
-static int xmp_truncate(const char *path, off_t size)
-{
-	int res;
-	char fpath[1000];
- 	sprintf(fpath,"%s%s", dirpath, path);
-    res = truncate(fpath, size);
-    if(res == -1)
-        return -errno;
-
-    return 0;
-}
-
+	
 static int xmp_write(const char *path, const char *buf, size_t size,
 		     off_t offset, struct fuse_file_info *fi)
 {
-	system("mkdir /home/ferdinand/Downloads/tmp/simpanan -p");
-	system("chmod 666 /homr/ferdinand/Downloads/tmp/simpanan");
 	int fd;
 	int res;
 	char fpath[1000];
-	char direktori[] = "/home/ferdinand/Downloads/tmp/simpanan/";
-	char command[1000];
-	sprintf(command,"touch %s%s",direktori,path);
-	system(command);
-    sprintf(fpath,"%s%s", direktori, path);
+    sprintf(fpath,"%s%s", dirpath, path);
 	(void) fi;
 	fd = open(fpath, O_WRONLY);
 	if (fd == -1)
@@ -120,6 +122,19 @@ static int xmp_write(const char *path, const char *buf, size_t size,
 
 	close(fd);
 	return res;
+}
+
+static int xmp_truncate(const char *path, off_t size)
+{
+    int res;
+     char fpath[1000];
+ 	sprintf(fpath,"%s%s", dirpath, path);
+
+    res = truncate(fpath, size);
+    if(res == -1)
+        return -errno;
+
+    return 0;
 }
 
 static int xmp_mknod(const char *path, mode_t mode, dev_t rdev)
@@ -134,28 +149,48 @@ static int xmp_mknod(const char *path, mode_t mode, dev_t rdev)
     return 0;
 }
 
-static int xmp_open(const char *path,struct fuse_file_info *fi)
+
+
+static int xmp_open(const char *path, struct fuse_file_info *fi)
 {
-	char command[1000];
-	int res;
-	char fpath[1000];
-    sprintf(fpath,"%s%s", dirpath, path);
-    res = open(fpath, fi->flags);
-    if (res == -1)
-        return -errno;
-    close(res);
-    return 0;
+    char fpath[1000];
+	if(strcmp(path,"/") == 0)
+	{
+		path=dirpath;
+		sprintf(fpath,"%s",path);
+	}
+	else sprintf(fpath, "%s%s",dirpath,path);
+
+        int res;
+        res = open(fpath, fi->flags);
+        if (res == -1)
+                return -errno;
+        close(res);
+        return 0;
 }
 
-static struct fuse_operations xmp_oper = {
-	.getattr	= xmp_getattr,
+
+static int xmp_utimens(const char *path, const struct timespec ts[2])
+{
+        int res;
+        /* don't use utime/utimes since they follow symlinks */
+        res = utimensat(0, path, ts, AT_SYMLINK_NOFOLLOW);
+        if (res == -1)
+                return -errno;
+        return 0;
+}
+
+  static struct fuse_operations xmp_oper = {
+  	.getattr	= xmp_getattr,
 	.readdir	= xmp_readdir,
-    .read		= xmp_read,
-    .truncate   = xmp_truncate,
-    .write      = xmp_write,
-	.mknod      = xmp_mknod,
+	.read		= xmp_read,
+	.rename     = xmp_rename,
+	.write 		= xmp_write,
+	.truncate	= xmp_truncate,
+	.mknod		= xmp_mknod,
 	.open		= xmp_open,
-};
+	.utimens 	= xmp_utimens,
+  };
 
 int main(int argc, char *argv[])
 {
